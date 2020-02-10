173c55a1893018be540c2118dcc6b94662f6caaf
Patrick <patrick.yrkim@gmail.com>
2020-02-10 15:31:43 -0800

    Update game start toggle

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import Game from './game';
 
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('game-canvas');
-    // const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
 
     // ctx.scale(20, 20);
 
     const GAME_WIDTH = canvas.width;
     const GAME_HEIGHT = canvas.height;
+
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    // ctx.fillStyle = '#5D5C61';
+    // ctx.fillStyle = '#7395AE';
+    // ctx.fillStyle = '3f';
+    ctx.fillStyle = '#98B4D4';
+    ctx.fill();
+
+    ctx.font = '18px arial';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.fillText("Press ENTER To Start", canvas.width / 2, canvas.height / 2);
 
     // const piece = new Piece;
     // const board = new Board(GAME_WIDTH, GAME_HEIGHT);
@@ -26,13 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
             game.piece.rotateAction();
         } else if (e.keyCode === 13) {
             game.toggleGameStart();
+            game.start();
         }
         // else if (e.keyCode === 32) {
         //     game.piece.fastDrop();
         // }
     });
 
-    game.start();
+    // game.start();
 });
 
 // // console.log("webpack is working")
